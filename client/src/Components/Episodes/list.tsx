c8a2c4348c07e0fa8e7abddc177afeda0e6190e0
NicholasCorcoran <39245904+NicholasCorcoran@users.noreply.github.com>
2020-01-29 17:15:43 -0500

    css updates and date display update

--- client/src/Components/Episodes/list.tsx
+++ client/src/Components/Episodes/list.tsx
@@ -3,18 +3,23 @@ import React from "react";
 interface ListProps {
   episode_id: any;
   title: any;
-  aired: any;
+  aired: string;
   filler: any;
 }
 
 export const List: React.FC<ListProps> = (props: ListProps) => {
+  let airedDate = props.aired;
+  if (airedDate !== null) {
+    airedDate = airedDate.slice(0, 10);
+  }
+
   return (
     <div className="info-wrapper">
       <div className="info">
         <ul>
           <li>Ep. Number: {props.episode_id}</li>
           <li>Title: {props.title}</li>
-          <li>Aired: {props.aired}</li>
+          <li>Aired: {airedDate}</li>
           <li>Filler: {props.filler}</li>
         </ul>
       </div>
